f79f797888576a4832f6af9d6e235ecaebd1dd1c
Luis Männer <info@luis-maenner.de>
2023-06-20 23:05:54 +0200

    chore(database): better database setup - run init script only if database does not exist - create performed_exercise table - create exercise table

--- src/installer/mysql/create-database-scheme.ts
+++ src/installer/mysql/create-database-scheme.ts
@@ -1,5 +1,5 @@
 import * as applicationConfig from '../../config/config.json';
-import { createConnection } from 'mysql2/promise';
+import { Connection, createConnection } from 'mysql2/promise';
 
 export async function createDatabaseScheme() {
     const databaseName = applicationConfig.database.database;
@@ -10,9 +10,23 @@ export async function createDatabaseScheme() {
         password: applicationConfig.database.password,
     });
 
-    await connection.execute(`CREATE DATABASE IF NOT EXISTS ${databaseName} CHARACTER SET utf8`);
+    const [createDatabaseResult] = await connection.execute(`CREATE DATABASE IF NOT EXISTS ${databaseName} CHARACTER SET utf8`);
+    if ((createDatabaseResult as unknown as { affectedRows: number }).affectedRows == 0) {
+        console.log('[INFO]: database already exists, skipping database initialization process.');
+        return;
+    }
+
     await connection.changeUser({ database: databaseName });
 
+    await createUserTable(connection);
+    await createRefreshTokenTable(connection);
+
+    await createExerciseTable(connection);
+
+    await createPerformedExerciseTable(connection);
+}
+
+async function createUserTable(connection: Connection) {
     await connection.execute(`
 		CREATE TABLE IF NOT EXISTS user(
 		    id MEDIUMINT UNSIGNED AUTO_INCREMENT,
@@ -25,7 +39,9 @@ export async function createDatabaseScheme() {
 		    PRIMARY KEY(id)
 		)
 	`);
+}
 
+async function createRefreshTokenTable(connection: Connection) {
     await connection.execute(`
 		CREATE TABLE IF NOT EXISTS refresh_token(
 		    user_id MEDIUMINT UNSIGNED,
@@ -35,5 +51,34 @@ export async function createDatabaseScheme() {
 		        REFERENCES user(id)
 		        ON DELETE CASCADE
 		)
+	`);
+}
+
+async function createExerciseTable(connection: Connection) {
+    await connection.execute(`
+		CREATE TABLE IF NOT EXISTS exercise(
+		    id TINYINT(255) UNSIGNED,
+		    name VARCHAR(255) UNIQUE,
+			description MEDIUMTEXT,
+		    PRIMARY KEY (id)
+		)
+	`);
+}
+
+async function createPerformedExerciseTable(connection: Connection) {
+    await connection.execute(`
+		CREATE TABLE IF NOT EXISTS performed_exercise(
+			user_id MEDIUMINT UNSIGNED,
+			exercise_id TINYINT(255) UNSIGNED,
+			repetitions SMALLINT UNSIGNED,
+			timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id)
+				REFERENCES user(id)
+				ON DELETE CASCADE
+		)
+	`);
+
+    await connection.execute(`
+		CREATE INDEX performed_exercise_user_exercise ON performed_exercise(user_id, exercise_id)
 	`);
 }
