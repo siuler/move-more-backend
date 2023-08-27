800c69264fff6b08daa3377aebdd6e165f8a2d9e
Luis Männer <info@luis-maenner.de>
2023-08-27 16:42:14 +0200

    fix(statistics): allow date format with one two digit month in requests

--- src/general/util.ts
+++ src/general/util.ts
@@ -1,6 +1,6 @@
 import { DateString } from '../domain/statistic/statistic';
 
-const datePattern = /^20[0-9]{2}-(:?[0-9]|10|11|12)-[0-3][0-9]$/;
+const datePattern = /^20[0-9]{2}-(:?(:?0?[0-9])|10|11|12)-[0-3][0-9]$/;
 
 export function validateDate(date: string): date is DateString {
     return datePattern.test(date);
