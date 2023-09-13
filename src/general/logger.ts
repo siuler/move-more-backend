cfd55b3967b55d6d48e04c163c993903c01f3c99
Luis Männer <info@luis-maenner.de>
2023-09-13 08:38:57 +0200

    feat(logging): implement pino logger

---
+++ src/general/logger.ts
@@ -0,0 +1,34 @@
+import pino from 'pino';
+
+export class Logger {
+    private static logger = pino();
+
+    public static info(...logMessages: unknown[]) {
+        this.logger.info(this.stringifyMessage(...logMessages));
+    }
+
+    public static warn(...logMessages: unknown[]) {
+        this.logger.warn(this.stringifyMessage(...logMessages));
+    }
+
+    private static stringifyMessage(...logMessages: unknown[]): string {
+        return logMessages
+            .map(message => {
+                switch (typeof message) {
+                    case 'string':
+                    case 'number':
+                    case 'bigint':
+                    case 'boolean':
+                    case 'symbol':
+                        return message;
+                    case 'object':
+                        return JSON.stringify(message);
+                    case 'function':
+                    case 'undefined':
+                    default:
+                        return typeof message;
+                }
+            })
+            .join(' ');
+    }
+}
