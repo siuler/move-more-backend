ace70cf7d5fb580400d3b037ef6f4edbcfb5579a
Luis Männer <info@luis-maenner.de>
2023-10-04 18:33:29 +0200

    refac(friend-notifications): use events for friend notifications

--- src/domain/messaging/push-notification/notification/push-notification-friend-already-worked-out.ts
+++ src/domain/messaging/push-notification/notification/push-notification-friend-already-worked-out.ts
@@ -3,13 +3,9 @@ import { PushNotification } from '../push-notification';
 
 export class PushNotificationFriendAlreadyWorkedOut extends PushNotification {
     constructor(exercise: Exercise, overtakerName: string, repCount: number) {
-        super(
-            `${exercise.pluralizedName}`,
-            `${overtakerName} already did ${repCount} ${exercise.pluralizedName}`,
-            'FRIEND_ALREADY_WORKED_OUT',
-            {
-                exerciseId: exercise.id.toString(),
-            }
-        );
+        const exerciseName = repCount == 1 ? exercise.name : exercise.pluralizedName;
+        super(exercise.pluralizedName, `${overtakerName} just did ${repCount} ${exerciseName}`, 'FRIEND_ALREADY_WORKED_OUT', {
+            exerciseId: exercise.id.toString(),
+        });
     }
 }
--- src/domain/messaging/push-notification/push-notification-internal-event-listener.ts
+++ src/domain/messaging/push-notification/push-notification-internal-event-listener.ts
@@ -2,10 +2,14 @@ import { InternalEventBus } from '../../../general/internal-event/event-bus';
 import { Exercise } from '../../exercise/exercise';
 import { ExerciseAbsolvedInternalEvent } from '../../exercise/exercise-absolved-internal-event';
 import { ExerciseService } from '../../exercise/exercise-service';
+import { FriendRequestAcceptedInternalEvent } from '../../exercise/friend-request-accepted-internal-event';
+import { FriendRequestSentInternalEvent } from '../../exercise/friend-request-sent-internal-event';
 import { RankingTimespans } from '../../ranking/ranking';
 import { RankingService } from '../../ranking/ranking-service';
 import { UserService } from '../../user/user-service';
 import { PushNotificationFriendAlreadyWorkedOut } from './notification/push-notification-friend-already-worked-out';
+import { PushNotificationFriendRequestAccepted } from './notification/push-notification-friend-request-accepted';
+import { PushNotificationFriendRequestReceived } from './notification/push-notification-friend-request-received';
 import { PushNotificationOvertakenByFriend } from './notification/push-notification-overtaken-by-friend';
 import { PushNotificationService } from './service/push-notification-service';
 
@@ -16,7 +20,21 @@ export class PushNotificationInternalEventListener {
         private exerciseService: ExerciseService,
         private userService: UserService
     ) {
+        InternalEventBus.on('friend-request-sent', this.handleFriendRequestSent.bind(this));
+        InternalEventBus.on('friend-request-accepted', this.handleFriendRequestAccepted.bind(this));
         InternalEventBus.on('exercise-absolved', this.handleExerciseAbsolved.bind(this));
+    }
+
+    private async handleFriendRequestSent(event: FriendRequestSentInternalEvent) {
+        const senderUsername = await this.userService.getUsername(event.data.senderId);
+        const notification = new PushNotificationFriendRequestReceived(senderUsername);
+        this.pushNotificationService.sendNotification(event.data.receiverId, notification);
+    }
+
+    private async handleFriendRequestAccepted(event: FriendRequestAcceptedInternalEvent) {
+        const receiverUsername = await this.userService.getUsername(event.data.receiverId);
+        const notification = new PushNotificationFriendRequestAccepted(receiverUsername);
+        this.pushNotificationService.sendNotification(event.data.senderId, notification);
     }
 
     private async handleExerciseAbsolved(event: ExerciseAbsolvedInternalEvent) {
