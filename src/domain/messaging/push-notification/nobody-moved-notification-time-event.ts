f8031061be46e146029f7df89ef89c9b5367efac
Luis Männer <info@luis-maenner.de>
2023-10-15 11:47:23 +0200

    fix(notification): correct event type for nobody moved event

--- src/domain/messaging/push-notification/nobody-moved-notification-time-event.ts
+++ src/domain/messaging/push-notification/nobody-moved-notification-time-event.ts
@@ -2,6 +2,6 @@ import { InternalEvent } from '../../../general/internal-event/internal-event';
 
 export class NobodyMovedNotificationTimeEvent extends InternalEvent {
     constructor() {
-        super('friend-request-accepted');
+        super('nobody-moved-notification-time');
     }
 }
